6cb1ffca4af72f2dfe893718976c7c83ff12592b
Trumane12311 <Trumane12311@gmail.com>
2021-06-14 20:02:28 -0400

    Creating variables

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,15 +1,17 @@
-let json = "data/samples.json"
+var selectID = d3.select("#selDataset");
+console.log(selectID)
+var demographics = d3.select("#sample-metadata");
+console.log(demographics)
+
+
+d3.json("data/samples.json")
+
 
 // Create the unpack data function
 function unpackData(rows, index) {
     return rows.map(function(row) {
         return row[index]
-    })
-}
+    });
+};
 
 // Testing build a plot function
-function buildPlot () {
-    d3.json(json).then(result => {
-        console.log(result);
-    })
-}
