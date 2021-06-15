2b4913e76e77eee5e139ee9680cbf5bd7aba907b
Trumane12311 <Trumane12311@gmail.com>
2021-06-15 19:21:55 -0400

    Creating task list for each chart piece

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,29 +1,68 @@
+// Select the user input field.
 let selectID = d3.select("#selDataset");
 selectID.on("change", function() {
     var node = select.node();
     console.log(node.value);
 });
-let demographics = d3.select("#sample-metadata");
+
+// Select the demographics for participant listing div.
+let demographicsPanel = d3.select("#sample-metadata");
 console.log(demographics);
+
+// Select bar chart div.
 let barChart = d3.select("bar");
 console.log(selectID);
-let gauge = d3.select("#guage");
-let bubble = d3.select("#bubble");
 
+// Select guage chart div.
+let gaugeChart = d3.select("#guage");
 
-let result = d3.json("samples.json").then(result => {
+// Select bubble chart div.
+let bubbleChart = d3.select("#bubble");
+
+// Import JSON file to view data
+let json = d3.json("samples.json").then(result => {
     console.log(result);
-})
+});
+
+/*  Import participant list into ul listing 
+for the dropdown menu.*/
 
 
 
-// Create the unpack data function
+/* Create a demographics panel that updates 
+based on the selected participant. It should 
+generate on the changing value of subject ul listing selection.*/
+
+
+
+/* Create a bar chart that updates 
+based on the selected participant.It should 
+generate on the changing value of subject ul listing selection.*/
+
+
+
+/* Create a guage chart that updates 
+based on the selected participant.It should 
+generate on the changing value of subject ul listing selection.*/
+
+
+
+/* Create a bubble chart that updates 
+based on the selected participant.It should 
+generate on the changing value of subject ul listing selection.*/
+
+
+
+
+
+
+/* Create the unpack data function
 function unpackData(rows, index) {
-   console.log(rows, index)
+   console.log(rows, index, "rows and index")
    return rows.map(function(row) {
        return row[index]   
     });
 };
-unpackData(result,0)
+unpackData(result,0)*/
 
 // Testing build a plot function
