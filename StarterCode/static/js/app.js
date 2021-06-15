174325ae4df0ea8a411facbdacc35f9c26dd9fac
Trumane12311 <Trumane12311@gmail.com>
2021-06-14 21:25:31 -0400

    Updated app.js, Completed initial variables

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,17 +1,22 @@
-var selectID = d3.select("#selDataset");
-console.log(selectID)
-var demographics = d3.select("#sample-metadata");
-console.log(demographics)
+let selectID = d3.select("#selDataset");
+console.log(selectID);
+let demographics = d3.select("#sample-metadata");
+console.log(demographics);
+let barChart = d3.select("bar");
+let gauge = d3.select("#guage");
+let bubble = d3.select("#bubble");
 
 
-d3.json("data/samples.json")
+d3.json("samples.json")
 
 
 // Create the unpack data function
-function unpackData(rows, index) {
-    return rows.map(function(row) {
-        return row[index]
-    });
-};
+// function unpackData(rows, index) {
+//     console.log(rows, index, "rows and index")
+//     return rows.map(function(row) {
+//         return row[index]
+//     });
+// };
+// unpackData()
 
 // Testing build a plot function
