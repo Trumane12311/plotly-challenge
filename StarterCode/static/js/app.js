19b82e48ff02e4b69b186239fd1ac54e99bf0f30
Trumane12311 <Trumane12311@gmail.com>
2021-06-15 12:25:47 -0400

    Making progress, JSON file now reading

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,22 +1,29 @@
 let selectID = d3.select("#selDataset");
-console.log(selectID);
+selectID.on("change", function() {
+    var node = select.node();
+    console.log(node.value);
+});
 let demographics = d3.select("#sample-metadata");
 console.log(demographics);
 let barChart = d3.select("bar");
+console.log(selectID);
 let gauge = d3.select("#guage");
 let bubble = d3.select("#bubble");
 
 
-d3.json("samples.json")
+let result = d3.json("samples.json").then(result => {
+    console.log(result);
+})
+
 
 
 // Create the unpack data function
-// function unpackData(rows, index) {
-//     console.log(rows, index, "rows and index")
-//     return rows.map(function(row) {
-//         return row[index]
-//     });
-// };
-// unpackData()
+function unpackData(rows, index) {
+   console.log(rows, index)
+   return rows.map(function(row) {
+       return row[index]   
+    });
+};
+unpackData(result,0)
 
 // Testing build a plot function
