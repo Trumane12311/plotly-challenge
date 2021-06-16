89439cfbea479ef977d1860f8fe118fec532e065
Trumane12311 <Trumane12311@gmail.com>
2021-06-16 17:07:11 -0400

    Updated plots functions to include bar and bubble charts

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,108 +1,78 @@
 function plots(subjectData) {
     d3.json("samples.json").then((data)=> {
         let subjects = data.samples;
-        console.log(subjects)
+        console.log(subjects);
         let resultsdata = subjects.filter(subject => subject.id == subjectData);
-        console.log(resultsdata)
+        console.log(resultsdata);
         let result = resultsdata[0];
         console.log(result);
         let ids = result.otu_ids;
-        console.log(ids)
+        console.log(ids);
         let labels = result.otu_labels;
-        console.log(labels)
+        console.log(labels);
         let values = result.sample_values;
-        console.log(values)
-});
-}
-        
-
+        console.log(values);
         
 // Create a bar chart that updates 
 // based on the selected participant.It should 
 // generate on the changing value of subject ul listing selection.
-/*        let traceBar = {
-            x: topTen,
-            y: OTUids,
-            text: OTUlabels,
+        let barchart =[
+            {
+            y:ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+            x:values.slice(0,10).reverse(),
+            text:labels.slice(0,10).reverse(),
             type:"bar",
-            orientation: "h",
-        };
-
-        let dataBar = [traceBar];
-
-        let layoutBar = {
-            title: "Top 10 Bacteria Cultures Found",
-            yaxis:{
-                tickmode:"linear",
+            orientation:"h"
             }
+        ];
+    
+        let barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: { t: 30, l: 150 }
         };
-
-        Plotly.newPlot("bar", dataBar, layoutBar);
+    
+        Plotly.newPlot("bar", barchart, barLayout);
+        })
         
-        //Bubble chart 
-        let traceBubble = {
-            x: bX,
-            y: bY,
+// Create a bubble chart that updates 
+// based on the selected participant.It should 
+// generate on the changing value of subject ul listing selection.
+        let bubbleLayout = {
+            margin: { t: 0 },
+            xaxis: { title: "OTU ID" },
+            hovermode: "closest",
+            };
+        
+        let bubbleData = [{
+            x: ids,
+            y: values,
+            text: labels,
             mode: "markers",
             marker: {
-                size: bSize,
-                color: bColor
-            },
-            text:bText
-        };
+            color: ids,
+            size: values,
+            }
+            }
+          ];
+        
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+};
+        
+// Create a function for the demographics panel that updates based on the selected participant.
 
-        let layoutBubble = {
-            title: "Bacteria Cultures Per Sample",
-            xaxis:{title: "OTU ID"},
-        };
-
-        let dataBubble = [traceBubble];
-
-        Plotly.newPlot("bubble", dataBubble, layoutBubble); 
-
-    //}));    
-//})}
-/*
-
-/* Create a function for the demographics panel that updates 
-based on the selected participant. It should 
-generate on the changing value of subject ul listing selection.*/
-
-function jsonData(id) {
+function panelInfo() {
     d3.json("samples.json").then((data) => {
         let metadata = data.metadata;
         console.log(metadata)
-
-        let result = metadata.filter(meta => meta.id.toString() === id)[0];
-
+        let resultsdata = metadata.filter(object => object.id == subject);
+        console.log(resultsdata);
+        let subject = resultsdata[0];
+        console.log(subject);
         let demographicPanel = d3.select("#sample-metadata");
         demographicPanel.html("");
-
-        Object.entries(result).forEach((key) => {   
-                demographicPanel.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");    
-        });
-    });
-}
-
-function updatePlots(newData) {
-    plots(newData);
-    jsonData(newData);
-};
-
-// select the user input field
-
-
-// select the demographic info div's ul list group
-let demographicsTable = d3.select("#sample-metadata");
-
-// select the bar chart div
-let barChart = d3.select("#bar");
-
-// select the bubble chart div
-let bubbleChart = d3.select("bubble");
-
-// select the gauge chart div
-let gaugeChart = d3.select("gauge");
+        Object.entries(subject).forEach(([key, value]) => {   
+                demographicPanel.append("h5").text(`${key}: ${value}`);
+});
 
 function init() {
     let selectID = d3.select("#selDataset");
@@ -113,11 +83,14 @@ function init() {
         }));
 
         // get the first ID from the list for initial charts as a default
-        const fillerData = subjectNames[0];
-        plots(fillerData);
-        buildMetadata(fillerData);
+        const firstSample = sampleNames[0];
+        plots(firstSample);
+        panelInfo(firstSample);
 
-    })); // close .then()
-}; // close init() function
+    }));
+}
 
-init();
+function updatePlots(newData) {
+    plots(newData);
+    panelInfo(newData);
+};
