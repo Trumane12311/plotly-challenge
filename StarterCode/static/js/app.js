7bfd511088b54db286719c5f0b08996cc8edb89d
Trumane12311 <Trumane12311@gmail.com>
2021-06-15 20:27:25 -0400

    Created functions, resetplot, started plotcharts function

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -7,22 +7,22 @@ selectID.on("change", function() {
 
 // Select the demographics for participant listing div.
 let demographicsPanel = d3.select("#sample-metadata");
-console.log(demographics);
+console.log(demographicsPanel);
 
 // Select bar chart div.
 let barChart = d3.select("bar");
 console.log(selectID);
 
-// Select guage chart div.
-let gaugeChart = d3.select("#guage");
+// Select guage chart div. 
+// let gaugeChart = d3.select("#guage");
 
 // Select bubble chart div.
 let bubbleChart = d3.select("#bubble");
 
 // Import JSON file to view data
-let json = d3.json("samples.json").then(result => {
-    console.log(result);
-});
+// let json = d3.json("samples.json").then(result => {
+//    console.log(result);
+// });
 
 /*  Import participant list into ul listing 
 for the dropdown menu.*/
@@ -33,6 +33,73 @@ for the dropdown menu.*/
 based on the selected participant. It should 
 generate on the changing value of subject ul listing selection.*/
 
+function init() {
+
+    // reset any previous data
+    resetData();
+
+    // read in samples from JSON file
+    d3.json("samples.json").then((result => {
+
+        result.names.forEach((name => {
+            let option = selectID.append("option");
+            option.text(name);
+        })); // close forEach
+
+        // get the first ID from the list for initial charts as a default
+        let updateID = selectID.property("value");
+
+        // plot charts with initial ID
+        plotCharts(updateId);
+
+    }));
+
+}
+
+function resetData() {
+    demographicsPanel.html("");
+    barChart.html("");
+    bubbleChart.html("");
+    // gaugeChart.html("");
+}
+
+init();
+
+function plotCharts(id) {
+
+    // read in the JSON data
+    d3.json("samples.json").then((data => {
+
+        // ----------------------------------
+        // POPULATE DEMOGRAPHICS TABLE
+        // ----------------------------------
+
+        // filter the metadata for the ID chosen
+        var individualSubject = data.metadata.filter(participant => participant.id == id)[0];
+
+        // get the wash frequency for gauge chart later
+        // var washfreq = individualMetadata.wfreq;
+
+        // Iterate through each key and value in the metadata
+        Object.entries(individualSubject).forEach(([key, value]) => {
+
+            var newList = demographicsTable.append("ul");
+            newList.attr("class", "list-group list-group-flush");
+
+            // append a li item to the unordered list tag
+            var subjectList = newList.append("li");
+
+            // change the class attributes of the list item for styling
+            subjectList.attr("class", "list-group-item p-1 demo-text bg-transparent");
+
+            // add the key value pair from the metadata to the demographics list
+            subjectList.text(`${key}: ${value}`);
+
+        });
+    }))
+
+//let individualSubject = json.metadata.filter(participant => participant.id == id)[0];
+//console.log(individualSubject);
 
 
 /* Create a bar chart that updates 
@@ -50,19 +117,3 @@ generate on the changing value of subject ul listing selection.*/
 /* Create a bubble chart that updates 
 based on the selected participant.It should 
 generate on the changing value of subject ul listing selection.*/
-
-
-
-
-
-
-/* Create the unpack data function
-function unpackData(rows, index) {
-   console.log(rows, index, "rows and index")
-   return rows.map(function(row) {
-       return row[index]   
-    });
-};
-unpackData(result,0)*/
-
-// Testing build a plot function
